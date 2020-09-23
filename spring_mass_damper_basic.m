2b16eedf3dee64ee7ab085c729b6d4cb9bed7b8a
sanjay <sanjaychaturvedi36@gmail.com>
2020-09-23 07:29:55 +0530

    P,PI and PID implementation completed

--- spring_mass_damper_basic.m
+++ spring_mass_damper_basic.m
@@ -5,17 +5,63 @@ b = 10; % N s/m
 k = 20; % N/m
 F = 1; % N
 
-% Transfer fucntion of basic spring mass damper system
+% Transfer fucntion(open loop) of basic spring mass damper system
 s = tf('s'); 
-Plant = 1/(m*s^2 + b*s + k);
+plant = 1/(m*s^2 + b*s + k);
 
 % open-loop step response
-figure(1);
-step(Plant);
+% figure(1);
+% step(plant);
 
-%open-loop step response at user defined time stamps
-t = 0:0.1:3;
-x = step(Plant,t);
+% open-loop step response at user defined time stamps
+t = 0:0.01:1.5;
+x = step(plant,t);
 
-figure(2);
-plot(t,x);
+% figure(2);
+% plot(t,x);
+
+% closed-loop system transfer function step-responses
+
+% P contoller
+kp = 300; 
+P = kp;
+plant_P = plant*P;
+plant_cl = feedback(plant_P,1);
+
+figure(3);
+x_p = step(plant_cl,t);
+plot(t,x_p);
+
+% PD controller
+kp = 300;
+kd = 10;
+PD = kp + kd*s;
+plant_PD = plant*PD;
+plant_cl = feedback(plant_PD,1);
+
+figure(4);
+x_pd = step(plant_cl,t);
+plot(t,x_pd);
+
+% PI controller
+kp = 300;
+ki = 70;
+PI = kp + ki/s;
+plant_PI = plant*PI;
+plant_cl = feedback(plant_PI,1);
+
+figure(5);
+x_pi = step(plant_cl,t);
+plot(t,x_pi);
+
+% PID controller
+kp = 350;
+ki = 300;
+kd = 50;
+PID = kp + kd*s + ki/s;
+plant_PID = plant*PID;
+plant_cl = feedback(plant_PID,1);
+
+figure(6);
+x_pid = step(plant_cl,t);
+plot(t,x_pid);
